5355e95452903384abbe4502238f675874d65a0c
Ricky Su <ricky@ez2.us>
2015-08-19 16:14:56 +0800

    add test

--- src/Tests/Parser/RequestParamParserTest.php
+++ src/Tests/Parser/RequestParamParserTest.php
@@ -22,4 +22,51 @@ class RequestParamParserTest extends BaseTestCase
         $this->assertEquals('some_data', $this->getObjectAttribute($parser, 'parseData'));
     }
 
+    public function test_forwardHook()
+    {
+        $parser1 = $this->getMock('WebUtil\\Parser\\RequestParamParser', array('feed'));
+        $parser1->expects($this->once())
+                ->method('feed')
+                ->willReturn(function($data){
+                    $this->assertEquals('some_data', $data);
+                });
+        $parser = new \WebUtil\Parser\RequestParamParser();
+        $parser->setNextHook($parser1);
+        $this->invokeObjectMethod($parser, 'forwardHook', array('some_data'));
+    }
+
+    public function test_feed_none_parsed()
+    {
+        $parser = $this->getMock('WebUtil\\Parser\\RequestParamParser', array('parse', 'forwardHook'));
+        $parser->expects($this->never())
+                ->method('forwardHook')
+                ->willReturn(null);
+        $parser->expects($this->exactly(3))
+                ->method('parse')
+                ->willReturn(null);
+        $parser->feed('a');
+        $parser->feed('b');
+        $parser->feed('c');
+        $this->assertEquals('abc', $this->getObjectAttribute($parser, 'rawData'));
+    }
+
+    public function test_feed_parsed()
+    {
+        $dataGlobal = '';
+        $parser = $this->getMock('WebUtil\\Parser\\RequestParamParser', array('parse', 'forwardHook'));
+        $parser->expects($this->exactly(3))
+                ->method('forwardHook')
+                ->willReturnCallback(function($data) use(&$dataGlobal){
+                    $dataGlobal.=$data;
+                });
+        $parser->expects($this->never())
+                ->method('parse')
+                ->willReturn(null);
+        $this->setObjectProperty($parser, 'parsed', true);
+        $parser->feed('a');
+        $parser->feed('b');
+        $parser->feed('c');
+        $this->assertEquals('abc', $dataGlobal);
+    }
+
 }
